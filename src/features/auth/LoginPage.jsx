a85004a02b58b643c973507f1e9cd7c6633cd36d
martiinfiierro2 <martinfierroachete@gmail.com>
2026-09-11 19:44:48 +0200

    Remove unused React import from login

--- src/features/auth/LoginPage.jsx
+++ src/features/auth/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { hasSession, loginUser, registerUser } from '../../services/authService';
 import Icon from '../../shared/Icon';
