79701c639528add677890d93f0243fd750c6fba8
martiinfiierro2 <martinfierroachete@gmail.com>
2026-09-11 19:41:21 +0200

    Remove obsolete recipes compatibility wrapper

--- src/componentes/recetas.jsx
+++
@@ -1,4 +0,0 @@
-// Entrada de compatibilidad: la pantalla y los datos ya viven en módulos separados.
-export { default } from '../features/recipes/RecipesPage';
-export { DEFAULT_RECIPES as recetasData } from '../data/defaultRecipes';
-export { getRecipes as obtenerRecetasGuardadas } from '../services/recipeService';
