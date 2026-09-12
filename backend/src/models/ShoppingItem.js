57029e4046c79d0a32f2881a1c1924cce3f559f4
martiinfiierro2 <martinfierroachete@gmail.com>
2026-09-12 12:13:22 +0200

    Castellanizar modelo de lista de compra

--- backend/src/models/ShoppingItem.js
+++ backend/src/models/ShoppingItem.js
@@ -1,16 +1,19 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
-const ShoppingItem = sequelize.define('ShoppingItem', {
+const ProductoCompra = sequelize.define('ProductoCompra', {
   id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
   nombre: { type: DataTypes.STRING(160), allowNull: false },
   cantidad: { type: DataTypes.STRING(60), allowNull: false, defaultValue: '1' },
   categoria: { type: DataTypes.STRING(60), allowNull: false, defaultValue: 'Otros' },
   comprado: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-  automatico: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
+  automatico: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+  usuarioId: { type: DataTypes.INTEGER.UNSIGNED, allowNull: false }
 }, {
-  tableName: 'shopping_items',
-  timestamps: true
+  tableName: 'lista_compra',
+  timestamps: true,
+  createdAt: 'creadoEn',
+  updatedAt: 'actualizadoEn'
 });
 
-export default ShoppingItem;
+export default ProductoCompra;
