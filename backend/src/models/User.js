0a42788913fb63ac04876cd0c6ac10c7e0b8a125
Martiinfierro2 <martinfierroachete@gmail.com>
2026-09-16 12:42:15 +0200

    añadir recordarios a perfil backend

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,7 +5,8 @@ const Usuario = sequelize.define('Usuario', {
   id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
   nombre: { type: DataTypes.STRING(80), allowNull: false },
   email: { type: DataTypes.STRING(160), allowNull: false, unique: true },
-  hashContrasena: { type: DataTypes.STRING(255), allowNull: false }
+  hashContrasena: { type: DataTypes.STRING(255), allowNull: false },
+  recordatorio: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true }
 }, {
   tableName: 'usuarios',
   timestamps: true,
